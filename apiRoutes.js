5bfcd6cecdf579e77ccfd2b7afb0a80ff8984192
SecuriumSolutions <71131093+SecuriumSolutions@users.noreply.github.com>
2020-10-21 17:45:34 +0530

    de

--- apiRoutes.js
+++
@@ -1,5 +0,0 @@
-const userADDController = require('../controllers/user/userAddQuery');
-module.exports = (app, express) => {
-    const apiRoute = express.Router();
-apiRoute.post('/adduser/', userADDController.addUserQuery);
-}
