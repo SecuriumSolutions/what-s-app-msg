cc537821d8fa3dd2a05d3e17aebd4a33f09e6aa4
SecuriumSolutions <71131093+SecuriumSolutions@users.noreply.github.com>
2020-10-21 17:44:01 +0530

    error fix

---
+++ apiRoutes.js
@@ -0,0 +1,5 @@
+const userADDController = require('../controllers/user/userAddQuery');
+module.exports = (app, express) => {
+    const apiRoute = express.Router();
+apiRoute.post('/adduser/', userADDController.addUserQuery);
+}
